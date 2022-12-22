64fb1eb8704229b0535505afd8e64e6fb4e44f63
David Bentiez <dbr.bcn@gmail.com>
2022-12-22 01:37:28 +0100

    add dynamic links

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -1,7 +1,9 @@
 import 'package:famlist/presentation/state/list_state.dart';
 import 'package:famlist/presentation/wigdet/famlist_drawer.dart';
 import 'package:famlist/services/lists_service.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
+import 'package:localization/localization.dart';
 
 import '../wigdet/products_list_view.dart';
 
@@ -34,6 +36,14 @@ class _MainPageState extends State<MainPage> {
             );
           },
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _generateShareLink(context);
+            },
+            icon: const Icon(Icons.share),
+          ),
+        ],
       ),
       drawer: const FamlistDrawer(),
       body: const ProductsListView(),
@@ -43,4 +53,35 @@ class _MainPageState extends State<MainPage> {
       ),
     );
   }
+
+  Future<Uri> _generateShareLink(BuildContext context) async {
+    final parameters = DynamicLinkParameters(
+      link: Uri.parse("https://famlist-app.web.app/${AppState.of(context).currentListId}"),
+      uriPrefix: "https://famlist.page.link",
+      androidParameters: AndroidParameters(
+        fallbackUrl: Uri.parse("https://play.google.com/store/apps/details?id=com.logicgear.famlist"),
+        packageName: "com.logicgear.famlist",
+        minimumVersion: 2,
+      ),
+      iosParameters: const IOSParameters(
+        bundleId: "com.logicgear.famlist",
+        appStoreId: "1660114766",
+        ipadBundleId: "com.logicgear.famlist",
+        minimumVersion: "1.0.1",
+      ),
+      googleAnalyticsParameters: const GoogleAnalyticsParameters(
+        source: "app",
+        medium: "social",
+        content: "share-",
+      ),
+      socialMetaTagParameters: SocialMetaTagParameters(
+        title: "app_name".i18n(),
+        description: "app_share_deep_link_description".i18n(),
+        imageUrl: Uri.parse("https://famlist-app.firebaseapp.com/img/logotype.4ab23ff2.jpg"),
+      ),
+    );
+    final shortLink = await FirebaseDynamicLinks.instance.buildShortLink(parameters);
+    return shortLink.shortUrl;
+  }
+
 }
